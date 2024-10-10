6fb90a3306573b813708ba12648ea05440ba3998
Asya <web.zaslavskaya@gmail.com>
2024-10-10 22:27:33 +0300

    site

---
+++ js/app.js
@@ -0,0 +1,47 @@
+const hiddenElements = document.querySelectorAll('.hidden-element');
+
+console.log(hiddenElements)
+
+if (window.innerWidth < 760) {
+  hiddenElements.forEach(element => {
+    element.classList.add('show');
+    element.classList.remove('hidden-element');
+  });
+} else {
+  const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add('show');
+              observer.unobserve(entry.target);
+          }
+      });
+  }, { threshold: 0.5 });
+
+  hiddenElements.forEach(element => {
+      observer.observe(element);
+  });
+}
+
+
+let menuLinks = document.querySelectorAll('.menu-link');
+
+
+menuLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+      
+      // Отримуємо ID секції, до якої потрібно скролити
+      let targetId = this.getAttribute('href');
+      let targetSection = document.getElementById(targetId.slice(1));
+      
+      if (targetId[0] == "#"){
+        e.preventDefault(); // Запобігаємо стандартній поведінці посилання
+        // console.log(targetId)
+        targetSection.scrollIntoView({
+          behavior: 'smooth', // Плавний скрол
+          block: 'start'
+        });
+      }
+   
+      // Скролимо до відповідної секції
+    });
+  });
